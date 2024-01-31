d847751730f7b361c7d6fccd9327c662782062fa
e129538 <Gabriel.Mendonca@mastercard.com>
2024-01-31 11:39:23 -0300

    reviewing some changes part 2

--- src/main/java/com/mastercard/dis/mids/reference/constants/TpVariables.java
+++ src/main/java/com/mastercard/dis/mids/reference/constants/TpVariables.java
@@ -1,72 +1,33 @@
 package com.mastercard.dis.mids.reference.constants;
 
 import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor(access= AccessLevel.PRIVATE)
 public class TpVariables {
 
+    @Setter
+    @Getter
     private static  String otpCode;
+    @Setter
+    @Getter
     private static  String emailCode;
+    @Setter
+    @Getter
     private static  String multiDocumentWorkflowId;
+    @Setter
+    @Getter
     private static  String workflowIdReAuth;
+    @Setter
+    @Getter
     private static  String aridValue;
+    @Setter
+    @Getter
     private static  String userProfileId;
+    @Setter
+    @Getter
     private static  String workflowId;
-
-    public static String getOtpCode() {
-        return otpCode;
-    }
-
-    public static void setOtpCode(String otpCode) {
-        TpVariables.otpCode = otpCode;
-    }
-
-    public static String getEmailCode() {
-        return emailCode;
-    }
-
-    public static void setEmailCode(String emailCode) {
-        TpVariables.emailCode = emailCode;
-    }
-
-    public static String getMultiDocumentWorkflowId() {
-        return multiDocumentWorkflowId;
-    }
-
-    public static void setMultiDocumentWorkflowId(String multiDocumentWorkflowId) {
-        TpVariables.multiDocumentWorkflowId = multiDocumentWorkflowId;
-    }
-
-    public static String getWorkflowIdReAuth() {
-        return workflowIdReAuth;
-    }
-
-    public static void setWorkflowIdReAuth(String workflowIdReAuth) {
-        TpVariables.workflowIdReAuth = workflowIdReAuth;
-    }
-
-    public static String getAridValue() {
-        return aridValue;
-    }
-
-    public static void setAridValue(String aridValue) {
-        TpVariables.aridValue = aridValue;
-    }
-
-    public static String getUserProfileId() {
-        return userProfileId;
-    }
-
-    public static void setUserProfileId(String userProfileId) {
-        TpVariables.userProfileId = userProfileId;
-    }
-
-    public static String getWorkflowId() {
-        return workflowId;
-    }
-
-    public static void setWorkflowId(String workflowId) {
-        TpVariables.workflowId = workflowId;
-    }
 }
